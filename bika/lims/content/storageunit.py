b1c141a663f0b08144cd6ee7f27d310cea66e6df
hocinebendou <hocinebendou@gmail.com>
2016-06-27 13:50:16 +0200

    some cleanup on contennt/storageunit

--- bika/lims/content/storageunit.py
+++ bika/lims/content/storageunit.py
@@ -1,22 +1,15 @@
 from AccessControl import ClassSecurityInfo
 from bika.lims import bikaMessageFactory as _
 from bika.lims import config
-from bika.lims.browser.widgets import ReferenceWidget as bika_ReferenceWidget
-from bika.lims.browser.widgets import RecordsWidget
 from bika.lims.content.bikaschema import BikaSchema
 from bika.lims.content.bikaschema import BikaFolderSchema
 from bika.lims.interfaces import IStorageUnit
-from DateTime.DateTime import DateTime
-from decimal import Decimal
 from plone.app.folder.folder import ATFolder
-from Products.Archetypes import atapi
 from Products.Archetypes.public import *
 from Products.Archetypes.references import HoldingReference
-from Products.ATContentTypes.content import base
-from Products.ATContentTypes.content import schemata
-from Products.ATExtensions.ateapi import RecordsField
 from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
+
 import sys
 
 schema = BikaFolderSchema.copy() + BikaSchema.copy() + Schema((
@@ -67,6 +60,7 @@ schema = BikaFolderSchema.copy() + BikaSchema.copy() + Schema((
 
 schema['description'].schemata = 'default'
 schema['description'].widget.visible = True
+
 
 class StorageUnit(ATFolder):
     security = ClassSecurityInfo()
