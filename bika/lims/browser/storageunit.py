ee9318f44b8f09691023d922bb9eabbe0f45167c
hocinebendou <hocinebendou@gmail.com>
2016-06-30 21:12:23 +0200

    remove add button from bika_storageinventories and add it in room

--- bika/lims/browser/storageunit.py
+++ bika/lims/browser/storageunit.py
@@ -1,10 +1,15 @@
 from bika.sanbi.controlpanel.bika_storageinventories import StorageInventoriesView
 from bika.sanbi.controlpanel.bika_storagemanagements import StorageManagementsView
+from bika.sanbi import bikaMessageFactory as _
 
 
 class StorageUnitInventoryView(StorageInventoriesView):
     def __init__(self, context, request):
         super(StorageUnitInventoryView, self).__init__(context, request)
+        self.contentFilter['getUnitID'] = context.getId()
+        self.context_actions = {_('Add'):
+                                {'url': 'createObject?type_name=StorageInventory',
+                                 'icon': '++resource++bika.lims.images/add.png'}}
 
 class StorageUnitManagementView(StorageManagementsView):
     def __init__(self, context, request):
